6a07be5946f166714b0dad4084075fa26f3c7d8b
Saadia Saleem <154665738+saadiasaleem67@users.noreply.github.com>
2024-02-23 14:22:24 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,19 @@
+// import inquirer from "inquirer"
+// let num = await inquirer.prompt({
+// type:"number",
+// name:"user",
+// message:"enter an amount"
+// })
+// const countdown = (second:number) =>{
+//     const interval = setInterval(()=>{
+//         console.log(`${second} seconds remaing`);
+//         second--;
+//         if(second<0){
+//             clearInterval(interval);
+//             console.log("countdown finish");  
+//         }
+//     },1000)
+// }
+// countdown(num.user)
+
+
